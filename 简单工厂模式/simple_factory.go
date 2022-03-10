173315a43b17c2632447e87d20b54a737b7fb6e7
qiankun <1975210542@qq.com>
2022-03-10 13:08:13 +0800

    简单工厂模式

--- 简单工厂模式/simple_factory.go
+++ 简单工厂模式/simple_factory.go
@@ -1,3 +1,35 @@
 package main
 
+import "fmt"
 
+type API interface {
+	Say(name string) string
+}
+
+func NewApi(name string) API {
+	switch name {
+	case "chinese":
+		return &Chinese{}
+	case "English":
+		return &English{}
+	}
+	return nil
+}
+
+type Chinese struct {
+}
+
+func (c *Chinese) Say(name string) string {
+	return name + "你好"
+}
+
+type English struct {
+}
+
+func (e *English) Say(name string) string {
+	return "hello " + name
+}
+func main() {
+	fmt.Println(NewApi("chinese").Say("乾坤"))
+	fmt.Println(NewApi("English").Say("乾坤"))
+}
